c778143a87e5dfec6b6a3db1079c74b7aa9d0447
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2018-10-19 15:12:29 -0700

    add symptom search

--- src/main.js
+++ src/main.js
@@ -14,10 +14,39 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      for (var i = 0; i < body.data.length; i++)
-      {
-        $(".list").append("<li>" + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "</li>");
+
+      if (body.meta.total !== 0) {
+        for (var i = 0; i < body.data.length; i++)
+        {
+          $(".list").append("<li>" + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "</li>");
+        }
       }
+      else {
+        $(".list").append("Sorry, there's no match for your search.");
+      }
+
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
+  $("#symptom-search-form").submit(function(event) {
+    event.preventDefault();
+    $("#result").show();
+    let symptomInput = $("#symptom").val();
+    let doctorList = new DoctorSearch();
+    let promise = doctorList.getDoctorBySymptoms(symptomInput);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+
+      if (body.meta.total !== 0) {
+        for (var i = 0; i < body.data.length; i++){
+          $(".list").append("<li>" + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "</li>");
+        }
+      } else {
+          $(".list").append("Sorry, there's no match for your search.")
+        }
 
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
